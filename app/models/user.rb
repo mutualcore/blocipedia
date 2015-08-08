a54ac8588229dbadea90030f8a390522b47d05f1
Lily Parmenter <parmenter.lily@gmail.com>
2015-08-07 23:34:38 -0500

    Added user roles with pundit and default to standard

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,21 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
   has_many :wikis
+  after_initialize :set_default_role
+
+  def standard?
+    role == 'standard'
+  end
+
+  def premium?
+    role == 'premium'
+  end
+
+  def admin?
+    role == 'moderator'
+  end
+
+  def set_default_role
+    self.role ||= 'standard'
+  end
 end
