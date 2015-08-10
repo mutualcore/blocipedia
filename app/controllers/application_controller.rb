9fd9a722bb7f1caa24c6828e95a18ad086ca77cf
Lily Parmenter <parmenter.lily@gmail.com>
2015-08-10 00:00:42 -0500

    made private wikis public on user downgrade

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,10 @@ class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  rescue_from Pundit::NotAuthorizedError do |exception|
+    redirect_to root_url, alert: exception.message
+  end 
+
   def after_sign_in_path_for(resource)
     wikis_path
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,8 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :confirmable
   has_many :wikis
   after_initialize :set_default_role
+
+  validates :role, presence: true, inclusion: { in: %w(standard premium admin), message: "should be one of admin, premium, standard" }
 
   def standard?
     role == 'standard'
@@ -30,5 +28,11 @@ class User < ActiveRecord::Base
   def downgrade_account(user)
     user.role = 'standard'
     user.save
+    user.wikis.where(private: true).update_all(private: false)
   end
+
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable
 end
